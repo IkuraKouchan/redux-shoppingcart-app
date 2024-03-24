2cd556a285ab702ed387dac1380d40a35b3b79f6
IkuraKouchan <k.t.r.111112@gmail.com>
2024-03-24 17:56:17 +0900

    first redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Navbar } from "./components/Navbar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <main>
+        <Navbar />
+      </main>
+    </div>
+  );
 }
 
 export default App;
